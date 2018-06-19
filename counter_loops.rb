087f2d017ef285ac0738c38cbf44042b7a8668ec
jli063 <jli063@rsdmo.org>
2018-06-19 20:28:17 +0000

    automatically backed up by learn

---
+++ counter_loops.rb
@@ -0,0 +1,22 @@
+def cha_cha_slide
+  moves_completed=0
+loop do
+  if moves_completed == 10
+    break
+  end
+  
+  puts "slide to the left"
+  sleep(0.1)
+  puts "slide to the right"
+  sleep(0.1)
+  puts "and kick"
+  
+
+
+moves_completed = moves_completed + 1
+
+puts "You've completed #{moves_completed} moves. Don't listen to what they say. You're a great dancer!"
+
+end
+
+end
