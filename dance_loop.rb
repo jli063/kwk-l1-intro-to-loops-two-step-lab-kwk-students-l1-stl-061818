291796ce395224236a2fd9282d941bdf5a0d8f0c
jli063 <jli063@rsdmo.org>
2018-06-19 20:06:18 +0000

    automatically backed up by learn

--- dance_loop.rb
+++ dance_loop.rb
@@ -1,11 +1,11 @@
 
 3.times do
   puts "Right foot back"
-  sleep(.5)
+  sleep(0.5)
   puts "Left foot back"
-  sleep(.5)
+  sleep(0.5)
   puts "Right foot back"
-  sleep(.5)
+  sleep(0.5)
   puts "Stop"
-  sleep(.5)
+  sleep(0.5)
 end
